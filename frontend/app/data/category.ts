65ca5b2fb07ff02e7e0d54e477f4dc41d3e74b4a
userjyoti <jyoti.bhola@grootsoftwares.com>
2026-02-18 17:20:03 +0530

    fixed folder structure

---
+++ frontend/app/data/category.ts
@@ -0,0 +1,8 @@
+export const categories = [
+  { id: 1, typeId: 1, name: "lipstick" },
+  { id: 2, typeId: 1, name: "Foundation" },
+  { id: 3, typeId: 2, name: "Moisturizer" },
+  { id: 4, typeId: 2, name: "Cleanser" },
+  { id: 5, typeId: 3, name: "Shampoo" },
+  { id: 6, typeId: 3, name: "Conditioner" },
+];
---
+++ frontend/app/data/types.ts
@@ -0,0 +1,10 @@
+export const types = [
+  { id: 1, name: "Makeup" },
+  { id: 2, name: "Skin" },
+  { id: 3, name: "Haircare" },
+  { id: 4, name: "Clearance Sale" },
+  { id: 5, name: "Bath & Body" },
+  { id: 6, name: "Luxe Store" },
+  { id: 7, name: "Fragrance" },
+  { id: 8, name: "Wellness" },
+];
